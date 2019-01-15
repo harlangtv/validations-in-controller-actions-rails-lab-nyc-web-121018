97f99deb785ab856cbadb34c7af4f8fe811afe55
github username <github email address>
2019-01-15 10:16:01 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
-  def show
+  def show #set_post method in before_action defines this method
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
-
+    if @post.update(post_params)
     redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
